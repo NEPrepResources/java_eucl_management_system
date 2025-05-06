c11fa091b31050cdc6ac499879f999fae86691df
Gisele-123 <giselemigisha53@gmail.com>
2025-05-06 21:55:57 +0200

    Initialized project

---
+++ src/main/java/com/eucl/eucl_management_system/EuclManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.eucl.eucl_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuclManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuclManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eucl/eucl_management_system/EuclManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eucl.eucl_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuclManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
